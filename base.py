af6dda98250ed2b65e5a95bfca407aefed201908
Rickson Rocha <roberto.rocha@bwa.global>
2023-09-05 15:37:03 -0300

    adicionando arquivo de locators

--- base.py
+++ base.py
@@ -1,3 +1,13 @@
+from utilities.bot_setup import BotSetup
+from selenium.webdriver.common.by import By
+from .locators import LOCATORS
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class EfiscoBase:
     
---
+++ locators.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+LOCATORS  = {
+    "BUTTON":{
+      
+    },
+    "PAINEL":{
+    
+    },
+    "INPUT":{
+      
+    },
+    "SELECT":{
+    }
+} 
