cd730510c5885d875d231bde39948c41e10f84c4
Rickson Rocha <roberto.rocha@bwa.global>
2023-09-14 14:10:19 -0300

    implementado métodos no arquivo base.py

--- base.py
+++ base.py
@@ -8,8 +8,44 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import logging
+from threading import Thread
+from images.images import IMAGES
+from utilities.pag import *
+import json
+import requests
+from time import sleep
 
-class EfiscoBase:
+import pandas as pd
+
+class EfiscoBase(BotSetup):
     
-    def abrir_sistema_efisco(self):
-        pass
+    def abrir_sistema_efisco(self): 
+        self.driver.get("https://efisco.sefaz.pe.gov.br/sfi_com_sca/PRMontarMenuAcesso")
+        logging.info("sistema efisco pronto para uso")
+
+    
+    def login_efisco(self):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(LOCATORS["BUTTON"]["ENTRAR_COM_GOV"]))
+        self.driver.find_element(*LOCATORS["BUTTON"]["ENTRAR_COM_GOV"]).click()
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(LOCATORS["BUTTON"]["LOGIN_CERT_DIGITAl"]))
+        certificado = (IMAGES["PAINEL"]["CERTIFICADO"])
+        self.__logar_com_certificado(certificado)
+        self.driver.find_element(*LOCATORS["BUTTON"]["LOGIN_CERT_DIGITAl"]).click()
+        logging.info("login com certificado realizado com sucesso")
+    
+    def __selecionar_certificado(self):
+        if exists(IMAGES["PAINEL"]["CERTIFICADO"]):
+            double_click(IMAGES["PAINEL"]["CERTIFICADO"])
+    
+    def __logar_com_certificado(
+        self,
+        base,
+        block_execution=False,
+    ):
+        certificate_selector = Thread(target=self.__selecionar_certificado, args=[])
+        certificate_selector.start()
+        if block_execution:
+            certificate_selector.join()
+    
+    
--- locators.py
+++ locators.py
@@ -2,14 +2,29 @@ from selenium.webdriver.common.by import By
 
 LOCATORS  = {
     "BUTTON":{
-      
+        "ENTRAR_COM_GOV": (By.CSS_SELECTOR,"#botao_gov_br > span"),
+        "LOGIN_CERT_DIGITAl": (By.CSS_SELECTOR,"#cert-digital > button"),
+        "LUPA":(By.CSS_SELECTOR,"#botao_busca"),
+        "LOCALIZAR": (By.CSS_SELECTOR,"#btt_localizar"),
+        "CAIXA_DE_ENTRADA":(By.CSS_SELECTOR,"#bt_consultar_caixas_mensagens")
     },
     "PAINEL":{
     
     },
+    "LOGO":{
+      "LOGO_EFISCO":(By.ID, "efisco_logo")
+    },
     "INPUT":{
-      
+      "BUSCA_EXTERNO": (By.CSS_SELECTOR,"#input_busca_geral"), 
+      "BUSCA_INTERNO": (By.CSS_SELECTOR,"#input_busca_geral_interno"),
+      "CNPJ_FORMULARIO": (By.CSS_SELECTOR,"#nuRadicalCNPJ")
     },
     "SELECT":{
-    }
-} 
+    },
+    "LINK":{
+      "DOM_ELETRONICO":(By.CSS_SELECTOR,"#busca_carrossel_itens > div > ul > li > a")
+    },
+    "TEXTS":{
+      "NAO_LIDA":(By.CSS_SELECTOR,"#table_tabeladados > tbody > tr.tabeladadosodd > td:nth-child(5)"),
+      "NAO_HA_REGISTRO": (By.CSS_SELECTOR,"#table_conteiner > tbody > tr:nth-child(40) > td > div > div.quantidade_registro > span")
+    }} 
